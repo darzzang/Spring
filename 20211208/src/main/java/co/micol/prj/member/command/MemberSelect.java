2af14f543ee766a7643d6cbd3c953025fbe68a1e
minjoo <aslwnw35@naver.com>
2021-12-09 12:04:36 +0900

    설명 추가

--- 20211208/src/main/java/co/micol/prj/member/command/MemberSelect.java
+++ 20211208/src/main/java/co/micol/prj/member/command/MemberSelect.java
@@ -14,14 +14,14 @@ public class MemberSelect implements Command {
 	@Override
 	public String run(HttpServletRequest request, HttpServletResponse response) {
 		
-		MemberService memberService = new MemberServiceImpl();	// 모델 호출
+		MemberService memberService = new MemberServiceImpl();	// 모델을 사용하기 위해 선언
 		MemberVO memberVO = new MemberVO();	// 값을 전달하거나 받기위해(한행의 레코드)
 		memberVO.setId(request.getParameter("id"));	// 폼에서 넘어온 아이디 값을 VO객체에 담음 
 		
-		memberVO = memberService.memberSelect(memberVO);
+		memberVO = memberService.memberSelect(memberVO);	// 모델을 호출
 		if(memberVO != null) {
-			request.setAttribute("member", memberVO);
-			returnPage = "member/memberSelect";
+			request.setAttribute("member", memberVO);	// 결과를 request 객체에 담음
+			returnPage = "member/memberSelect";	// 돌려줄 페이지 설정
 		} else {
 			request.setAttribute("message", "회원 세부 정보 불러오기 실패");
 			returnPage = "member/memberFail";
